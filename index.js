fc550910d03e5dd0e711fac04618388f27c50206
Lactor <lactorjm4@gmail.com>
2023-08-07 23:35:44 +0300

    Completed Project Phase 1

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
+const dbJsonUrl = './db.json'; 
 
-const imageUrls = {
-    
-};
-
-const imageElements = document.querySelectorAll(".image img");
-const selectButtons = document.querySelectorAll(".select-btn");
-const formContainer = docuement.querySelector(".form-container");
+const bannerImage = document.querySelector('.banner img');
+const imageElements = document.querySelectorAll('.image img');
+const selectButtons = document.querySelectorAll('.select-btn');
+const formContainer = document.querySelector('.form-container');
+const banner = document.querySelector('.banner');
 
 let currentIndex = 0;
 
-function fetchImages(){
+function loadImages(images) {
+    bannerImage.src = images[0];
+
     imageElements.forEach((img, index) => {
-        img.src = imageUrls[index];
+        img.src = images[index + 1];
     });
 }
-function moveToNextImage(){
-    currentIndex = (currentIndex + 1) % imageUrls.length;
-    fetchImages();
+
+function moveToNextImage(data) {
+    currentIndex = (currentIndex + 1) % data.images.length;
+    loadImages(data.images);
 }
-function showForm(){
-    formContainer.style.display = "block";
+
+function showForm() {
+    formContainer.style.display = 'block';
 }
-function handleSubmit(event){
+
+function handleSubmit(event) {
     event.preventDefault();
-    alert("Thank you for your review/suggetion!");
+    alert('Thank you for your review/suggestion!');
 }
-selectButtons.forEach((button) => {
-    button.addEventListener("click",showForm);
-});
-formContainer.addEventListener("submit", handleSubmit);
-fetchImages();
-const banner = document.querySelector(".banner");
-banner.addEventListener("click", moveToNextImage);
+
+selectButtons.forEach(button => button.addEventListener('click', showForm));
+formContainer.addEventListener('submit', handleSubmit);
+
+banner.addEventListener('click', () => moveToNextImage(currentIndex));
+
+fetch(dbJsonUrl)
+    .then(response => response.json())
+    .then(data => {
+        loadImages(data.images);
+        banner.addEventListener('click', () => moveToNextImage(data));
+    })
+    .catch(error => console.error('Error fetching images:', error));
